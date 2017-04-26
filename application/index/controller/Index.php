68caf6e2166e2e7ab383e23d1511ccaefed3e2f1
dinglf1994 <13120310025@163.com>
2017-04-26 08:12:20 +0800

    classification

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,4 +9,19 @@ class Index extends Controller
     {
         return $this->fetch();
     }
+    public function dirList()
+    {
+        // 食品
+        $dir = CLASSIFICATION_PATH. 'shipin/';
+        $file = scandir($dir);
+//        print_r($file);
+        unset($file[0]); unset($file[1]);
+
+        foreach ($file as $value) {
+            $data = file_get_contents($dir. $value);
+            if (strlen($data) > 100) {
+                file_put_contents(CLASSIFICATION_PATH. 'pinyin2/'. $value, $data);
+            }
+        }
+    }
 }
